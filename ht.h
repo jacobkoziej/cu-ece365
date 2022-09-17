5bb970c923b3ba19a7e911cf635e48d7f2543d2a
Jacob Koziej <jacobkoziej@gmail.com>
2022-09-17 16:54:04 -0400

    Define `htt` class

--- ht.h
+++ ht.h
@@ -51,5 +51,59 @@ public:
 	int  set(const void *key, std::size_t siz, void *val);
 };
 
+template <typename T>
+class htt {
+	ht p_ht;
+
+	const void *(*key)(const T &key) = nullptr;
+	std::size_t (*siz)(const T &key) = nullptr;
+
+public:
+	htt(const void *(*key)(const T &key), std::size_t (*siz)(const T &key));
+
+	inline bool exists(const T &key) const;
+	inline int  get(const T &key, void **val) const;
+	inline int  insert(const T &key, void *val = nullptr);
+	inline int  rm(const T &key);
+	inline int  set(const T &key, void *val);
+};
+
+template <typename T>
+htt<T>::htt(const void *(*key)(const T &key), std::size_t (*siz)(const T &key))
+{
+	this->key = key;
+	this->siz = siz;
+}
+
+template <typename T>
+inline bool htt<T>::exists(const T &key) const
+{
+	return p_ht.exists(this->key(key), this->siz(key));
+}
+
+template <typename T>
+inline int htt<T>::get(const T &key, void **val) const
+{
+	return p_ht.get(this->key(key), this->siz(key), val);
+}
+
+template <typename T>
+inline int htt<T>::insert(const T &key, void *val)
+{
+	return p_ht.insert(this->key(key), this->siz(key), val);
+}
+
+template <typename T>
+inline int htt<T>::rm(const T &key)
+{
+	return p_ht.rm(this->key(key), this->siz(key));
+}
+
+template <typename T>
+inline int htt<T>::set(const T &key, void *val)
+{
+	return p_ht.set(this->key(key), this->siz(key), val);
+}
+
 
 #endif /* CU_ECE365_HT_H */
