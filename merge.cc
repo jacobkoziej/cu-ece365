ebebe65783908875f9f6e21a46db6576b4f0bbbd
Jacob Koziej <jacobkoziej@gmail.com>
2022-11-21 21:01:56 -0500

    Remove need for a `merge` class

--- merge.cc
+++ merge.cc
@@ -17,65 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "merge.h"
-
 #include <cstdlib>
-#include <utility>
-
-
-void merge::delete_bitmap(void)
-{
-	for (std::size_t i = 0; i < ydim; i++)
-		delete[] bitmap[i];
-
-	delete[] bitmap;
-
-	bitmap = nullptr;
-}
-
-
-merge::merge(void)
-{
-	bitmap = nullptr;
-}
-
-merge::merge(std::string &a, std::string &b)
-{
-	bitmap = nullptr;
-
-	set_strings(a, b);
-}
-
-merge::~merge(void)
-{
-	if (!bitmap) return;
-
-	delete_bitmap();
-}
-
-void merge::set_strings(std::string &a, std::string &b)
-{
-	if (bitmap) delete_bitmap();
-
-	this->a = a;
-	this->b = b;
-
-	// we want to use make sure string `a` is the larger of the two
-	// since it will increase the efficiency of the bit-packing
-	if (this->a.size() < this->b.size()) std::swap(this->a, this->b);
-
-	xdim = this->a.size() + 1;
-	ydim = this->b.size() + 1;
-
-	bitmap = new bitmap_t*[ydim];
-
-	std::size_t xdim_siz = xdim / (sizeof(bitmap_t) << 3);
-	if (xdim & 0x07) ++xdim_siz;
-
-
-	for (std::size_t i = 0; i < ydim; i++)
-		bitmap[i] = new bitmap_t[xdim_siz];
-}
 
 
 int main(void)
